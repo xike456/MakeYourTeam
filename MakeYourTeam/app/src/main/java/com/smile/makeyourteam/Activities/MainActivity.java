bbb22438f87e5656657e17d3a65b2496ad96c76e
letanvu <letanvu.it@gmail.com>
2016-11-15 22:17:55 +0700

    login facebook, google

--- MakeYourTeam/app/src/main/java/com/smile/makeyourteam/Activities/MainActivity.java
+++ MakeYourTeam/app/src/main/java/com/smile/makeyourteam/Activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.smile.makeyourteam.Activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -7,24 +9,73 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.smile.makeyourteam.Fragments.ChatGroupFragment;
 import com.smile.makeyourteam.Fragments.ChatManagerFragment;
 import com.smile.makeyourteam.Fragments.HomeFragment;
 import com.smile.makeyourteam.Fragments.SettingsFragment;
 import com.smile.makeyourteam.Fragments.TaskManagerFragment;
 import com.smile.makeyourteam.R;
+import com.smile.makeyourteam.server.Firebase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private MyAdapter adapter;
 
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // init FacebookSDK
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
         setContentView(R.layout.activity_main);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = Firebase.firebaseAuth.getCurrentUser();
+                if (user != null) {
+                   // LoadUser();
+                    // LoadGroups();
+                    Log.d("Authentication",  "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    StartLogin();
+                    Log.d("Authentication", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -62,7 +113,60 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.logout){
+            logOut();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Firebase.firebaseAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            Firebase.firebaseAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    // function call activity login
+    private void StartLogin() {
+        Intent login = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(login);
+        finish();
+    }
+
+    // function user logout
+    private void logOut() {
+        // logout firebase
+        Firebase.firebaseAuth.signOut();
+        // logout facebook
+        LoginManager.getInstance().logOut();
+        // Google sign out
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                    }
+                });
+    }
+
+    // class adapter tab botttom
     class MyAdapter extends FragmentPagerAdapter {
 
         Fragment fragments[] = {
@@ -94,5 +198,12 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return titles[position];
         }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ MakeYourTeam/app/src/main/java/com/smile/makeyourteam/Config.java
@@ -0,0 +1,12 @@
+package com.smile.makeyourteam;
+
+/**
+ * Created by NgoChiHai on 10/26/16.
+ */
+
+public class Config {
+    public static final String GROUP_INFO = "group_info";
+    public static String ID_USER_LIST = "user_id";
+    public static String ID_GROUP = "group_id";
+    public static String NAME_USER_RECEIVE = "name_user_receive";
+}
